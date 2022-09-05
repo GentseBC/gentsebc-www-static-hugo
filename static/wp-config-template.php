3befb54422a5e5225a8817370f896e8b9f979580
thomas.dekeyser <thomas.dekeyser@gmail.com>
2022-09-06 00:43:49 +0200

    Fix ; in php

--- static/wp-config-template.php
+++ static/wp-config-template.php
@@ -17,7 +17,7 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-define('RECAPTCHA_SITE_SECRET','%%recaptchasitesecret%%')
-define('FORM_NOTIFICATION_TO_ADDRESS','%%formnotificationtoaddress%%')
+define('RECAPTCHA_SITE_SECRET','%%recaptchasitesecret%%');
+define('FORM_NOTIFICATION_TO_ADDRESS','%%formnotificationtoaddress%%');
 
 
