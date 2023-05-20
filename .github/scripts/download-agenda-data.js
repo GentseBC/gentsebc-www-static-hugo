a0d6d78467ce186d8696173b8162f541d00c5144
Thomas Dekeyser <thomas.dekeyser@noreply.com>
2023-05-20 21:39:46 +0200

    Short-term-calendar workflow (trial/error)

--- .github/scripts/download-agenda-data.js
+++ .github/scripts/download-agenda-data.js
@@ -1,4 +1,4 @@
-module.exports = async ({github, context, io, fetch}) => {
+module.exports = async ({github, context, io, fetch, dayjs}) => {
     console.log("Starting downloading from google");
 
     function dateToYMD(date) {
@@ -23,8 +23,67 @@ module.exports = async ({github, context, io, fetch}) => {
     URL = 'https://www.googleapis.com/calendar/v3/calendars/gentsebc%40gmail.com/events?orderBy=startTime&q=speelmoment&singleEvents=true&timeMax=' + dateToYMD(toDate) +'T00%3A00%3A00-00%3A00&timeMin='+ dateToYMD(fromDate) + 'T00%3A00%3A00-00%3A00&key=AIzaSyBRQRMJ_sZC4vIiPbtvyscTaXWknlp7Pak';
     console.log(URL);
     let calendarData = await fetchAsync(URL);
-    console.log(calendarData);
+
+    function resolveEventType(summary) {
+        if (summary === undefined) {
+            return undefined
+        }
+        lowerSummary = summary.toLowerCase();
+
+        if (lowerSummary.indexOf("jeugd") !== -1) {
+            return "youthCalItems";
+        } else if (lowerSummary.indexOf("volwassenen") !== -1) {
+            return "adultCalItems";
+        } else if (lowerSummary.indexOf("G-sport") !== -1) {
+            return "gSportCalItems";
+        } 
+        return undefined;
+    }
+
+    function resolveLocationCode(location) {
+        if (location === undefined) {
+            return "other";
+        }
+        lowerLocation = location.toLowerCase();
+
+        if (lowerLocation.indexOf("merckx") !== -1) {
+            return "wielerpiste";
+        } else if (lowerLocation.indexOf("bourgoyen") !== -1) {
+            return "bourgoyen";
+        } else if (lowerLocation.indexOf("topsporthal") !== -1) {
+            return "topsporthal";
+        } else {
+            return "other";
+        }
+    }
+    
+    result = [];
+    dayjs.locale('nl-be');
+    const dateToIndex= new Map();
+    for(let i = 0; i < numberOfDaysToDisplay; i++) {
+        const aDay = {"adultCalItems":[],"youthCalItems":[],"gSportCalItems":[]};
+        const myDay = dayjs().add(i, 'day');
+        aDay.day =  myDay.format('dd D MMM')
+        dateToIndex.set(myDay.format('YYYY-MM-DD'), i);
+        result.push(aDay);
+    }
 
 
-    return context;
+    remoteCalEvents.items
+            .filter(item => item.start !== undefined && item.start.dateTime !== undefined && item.end !== undefined && item.end.dateTime !== undefined  && item.summary !== undefined)
+            .forEach(item => {
+                const dateIndex = dateToIndex.get(item.start.dateTime.substring(0,10));
+                const evenType = resolveEventType(item.summary);
+                if (dateToIndex !== undefined && evenType !== undefined)  {
+                    result[dateIndex][evenType].push({
+                        "startDateTime": dayjs(item.start.dateTime).format("YYYY-MM-DD HH:mm:ss"),
+                        "endDateTime": dayjs(item.end.dateTime).format("YYYY-MM-DD HH:mm:ss"),
+                        "location": item.location,
+                        "locationCode": resolveLocationCode(item.location)
+                    });
+                }
+    });
+    console.log(result); 
+
+    return result;
 }
