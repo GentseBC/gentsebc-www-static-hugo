ed4af0613eb9e68c5c069c308905d08a74ad1700
Thomas Dekeyser <thomas.dekeyser@noreply.com>
2023-06-24 22:25:00 +0200

    Move to 3 different kalendars

--- .github/scripts/download-agenda-data.js
+++ .github/scripts/download-agenda-data.js
@@ -10,23 +10,17 @@ module.exports = async ({github, core, context, io, fetch, dayjs}) => {
     }
 
     async function fetchAll(fromDate,toDate) {
-        const [volwassenenResponse, jeugdResponse, gsportResponse] = await Promise.all([
+        const [adultResponse, youthResponse, gsportResponse] = await Promise.all([
                                                                            fetch('https://www.googleapis.com/calendar/v3/calendars/gentsebc%40gmail.com/events?orderBy=startTime&q=speelmoment&singleEvents=true&timeMax=' + dateToYMD(toDate) +'T00%3A00%3A00-00%3A00&timeMin='+ dateToYMD(fromDate) + 'T00%3A00%3A00-00%3A00&key=AIzaSyBRQRMJ_sZC4vIiPbtvyscTaXWknlp7Pak'),
                                                                            fetch('https://www.googleapis.com/calendar/v3/calendars/gentsebc%40gmail.com/events?orderBy=startTime&q=speelmoment&singleEvents=true&timeMax=' + dateToYMD(toDate) +'T00%3A00%3A00-00%3A00&timeMin='+ dateToYMD(fromDate) + 'T00%3A00%3A00-00%3A00&key=AIzaSyBRQRMJ_sZC4vIiPbtvyscTaXWknlp7Pak'),
                                                                            fetch('https://www.googleapis.com/calendar/v3/calendars/gentsebc%40gmail.com/events?orderBy=startTime&q=speelmoment&singleEvents=true&timeMax=' + dateToYMD(toDate) +'T00%3A00%3A00-00%3A00&timeMin='+ dateToYMD(fromDate) + 'T00%3A00%3A00-00%3A00&key=AIzaSyBRQRMJ_sZC4vIiPbtvyscTaXWknlp7Pak')
                                                                        ]);
 
-        const volwassenen = await volwassenenResponse.json();
-        const jeugd = await jeugdResponse.json();
+        const adult = await adultResponse.json();
+        const youth = await youthResponse.json();
         const gsport = await gsportResponse.json();
 
-        return [volwassenen, jeugd, gsport];
-    }
-
-    async function fetchAsync (url) {
-        let response = await fetch(url);
-        let data = await response.json();
-        return data;
+        return [adult, youth, gsport];
     }
 
     function resolveLocationCode(location) {
@@ -51,7 +45,7 @@ module.exports = async ({github, core, context, io, fetch, dayjs}) => {
             .filter(item => item.start !== undefined && item.start.dateTime !== undefined && item.end !== undefined && item.end.dateTime !== undefined  && item.summary !== undefined)
             .forEach(item => {
                 const dateIndex = dateToIndex.get(item.start.dateTime.substring(0,10));
-                if (dateToIndex !== undefined && evenType !== undefined)  {
+                if (dateToIndex !== undefined)  {
                     result[dateIndex][evenType].push({
                                                          "startDateTime": dayjs(item.start.dateTime).tz("Europe/Brussels").format("YYYY-MM-DD HH:mm:ss"),
                                                          "endDateTime": dayjs(item.end.dateTime).tz("Europe/Brussels").format("YYYY-MM-DD HH:mm:ss"),
@@ -67,7 +61,7 @@ module.exports = async ({github, core, context, io, fetch, dayjs}) => {
         const dateToIndex= new Map();
         for(let i = 0; i < numberOfDaysToDisplay; i++) {
             const aDay = {"adultCalItems":[],"youthCalItems":[],"gSportCalItems":[]};
-            const myDay = dayjs().add(i, 'day');
+            const myDay = dayjs("2023-06-12T00:00:01.000Z").add(i, 'day'); // TESTING
             aDay.day =  myDay.format('dd D MMM')
             dateToIndex.set(myDay.format('YYYY-MM-DD'), i);
             result.push(aDay);
